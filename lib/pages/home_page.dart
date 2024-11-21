323baccea0e8046dedc1090616765c7e63d10808
SotaAsai <sota.asai.28@gmail.com>
2024-11-21 15:18:28 +0900

    ホーム画面のユーザーリスト作成

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
-
+import 'package:firebase_chat_app_tutorial/models/user_profile.dart';
 import 'package:firebase_chat_app_tutorial/services/alert_service.dart';
 import 'package:firebase_chat_app_tutorial/services/auth_service.dart';
+import 'package:firebase_chat_app_tutorial/services/database_service.dart';
 import 'package:firebase_chat_app_tutorial/services/navigation_service.dart';
+import 'package:firebase_chat_app_tutorial/widgets/chat_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
@@ -18,6 +20,7 @@ class _HomePageState extends State<HomePage> {
   late AuthService _authService;
   late NavigationService _navigationService;
   late AlertService _alertService;
+  late DatabaseService _databaseService;
 
   @override
   void initState() {
@@ -25,6 +28,7 @@ class _HomePageState extends State<HomePage> {
     _authService = _getIt<AuthService>();
     _navigationService = _getIt<NavigationService>();
     _alertService = _getIt<AlertService>();
+    _databaseService = _getIt<DatabaseService>();
   }
 
   @override
@@ -34,25 +38,71 @@ class _HomePageState extends State<HomePage> {
         title: const Text(
           "Messages",
         ),
-          actions: [
-            IconButton(
-              color: Colors.red,
-              onPressed: () async {
-                bool result = await _authService.logout();
-                if (result) {
-                  _alertService.showToast(
-                    text: "Successfully logged out!",
-                    icon: Icons.check,
-                  );
-                  _navigationService.pushReplacementNamed("/login");
-                }
-              },
-              icon: const Icon(
-                Icons.logout,
-              ),
+        actions: [
+          IconButton(
+            color: Colors.red,
+            onPressed: () async {
+              bool result = await _authService.logout();
+              if (result) {
+                _alertService.showToast(
+                  text: "Successfully logged out!",
+                  icon: Icons.check,
+                );
+                _navigationService.pushReplacementNamed("/login");
+              }
+            },
+            icon: const Icon(
+              Icons.logout,
             ),
-          ],
+          ),
+        ],
+      ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15.0,
+          vertical: 20.0,
+        ),
+        child: _chatsList(),
       ),
     );
+  }
+
+  Widget _chatsList() {
+    return StreamBuilder(
+        stream: _databaseService.getUserProfiles(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Unable to load data."),
+            );
+          }
+          if (snapshot.hasData && snapshot.data != null) {
+            final users = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                UserProfile user = users[index].data();
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                  ),
+                  child: ChatTile(
+                    userProfile: user,
+                    onTap: () {},
+                  ),
+                );
+              },
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,12 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_chat_app_tutorial/models/user_profile.dart';
+import 'package:firebase_chat_app_tutorial/services/auth_service.dart';
+import 'package:get_it/get_it.dart';
 
 class DatabaseService {
+  final GetIt _getIt = GetIt.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  late AuthService _authService;
 
   CollectionReference? _usersCollection;
 
   DatabaseService() {
+    _authService = _getIt.get<AuthService>();
     _setupCollectionReferences();
   }
 
@@ -18,5 +24,11 @@ class DatabaseService {
 
   Future<void> createUserProfile({required UserProfile userProfile}) async {
     await _usersCollection?.doc(userProfile.uid).set(userProfile);
+  }
+
+  Stream<QuerySnapshot<UserProfile>> getUserProfiles() {
+    return _usersCollection
+      ?.where("uid", isNotEqualTo: _authService.user!.uid)
+      .snapshots() as Stream<QuerySnapshot<UserProfile>>;
   }
 }
